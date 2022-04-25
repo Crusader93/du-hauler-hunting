2c704b03ab0690977919270c1484a8a2a61c5c28
Crusader93 <squadron314@gmail.com>
2022-04-25 19:40:08 +0300

    added camera api pos detection based on radar selected target range

--- DUPIRATE/du-pos-camera-api.lua
+++ DUPIRATE/du-pos-camera-api.lua
@@ -1,10 +1,11 @@
-function missionTax(system)
+function missionTax(system,radar)
+    local id = radar.getTargetId()
+    local dist = radar.getConstructDistance(id)
     local forwvector = vec3(system.getCameraWorldForward())
     local worldpos = vec3(system.getCameraWorldPos())
     local pos = (dist * forwvector + worldpos)
-    local x, y, z = pos:unpack()
-    local wayp = "::pos{0,0," .. x .. "," .. y .. "," .. z .. "}"
+    local x,y,z = pos:unpack()
+    local wayp = '::pos{0,0,'..x..','..y..','..z..'}'
     system.setWaypoint(wayp)
-    system.print(wayp)
-    return wayp
+return wayp
 end
